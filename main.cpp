3dfcd3ed0c91c75839498a3d79b9d1aebe8b1160
bretw84 <bretw84@gmail.com>
2019-10-22 18:00:55 -0500

    I only know small amount of c++... added propmt to input name, and display name on first message. Maybe it could be used throughout your game?

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 /* obviously main everything place */
 #include <iostream>
+using namespace std;
 int main()
 {
-	std::cout << "This is going to be main loop\n";
+        string name;
+        cout << "Please enter your name\n";
+        cin >> name;
+        cout << "This is going to be main loop, " << name;
+        cout << "\n\n";
 }
