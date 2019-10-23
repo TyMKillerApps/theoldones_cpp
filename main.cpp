4a6ea6ec76c818b802b9bff190948e8d99c22947
Tyler Poush <hooch0022@gmail.com>
2019-10-22 20:03:04 -0500

    first iteration of game_loop inside main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 /* obviously main everything place */
 #include <iostream>
+using namespace std;
 int main()
 {
-	std::cout << "This is going to be main loop\n";
+	bool hero_choice_1 {false};
+        do{
+		cout << "\nLoop Still Going.. \nKeep Going?\n";
+		cin >> hero_choice_1;
+		if ( hero_choice_1 )
+		{
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	while (!hero_choice_1);
+return 0;
 }
