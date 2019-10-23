90a5731801523a867350fd6852362f644d85c171
Tyler Poush <15953749+TyMKillerApps@users.noreply.github.com>
2019-10-23 00:12:41 -0500

    added dice rolling tests to main

--- dice.h
+++ dice.h
@@ -1,1 +1,20 @@
 /* dice class collections */
+#include <cstdlib>
+
+class fair_dice
+{
+	public: int sided_dice;
+	public: int number_of_die;
+	/*int randomNumber = rand();*/
+	void roll_dice( int number_of_die, int sided_dice)
+	{
+		int i = 0;
+		do
+		{
+		int randomNumber = rand();
+		std::cout << ((randomNumber %(sided_dice)) +1) << std::endl;
+		++i;
+		}
+		while(i < number_of_die);
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 /* obviously main everything place */
 #include <iostream>
+#include "dice.h"
 using namespace std;
 int main()
 {
 	bool hero_choice_1 {false};
 	int menu_choice;
+	fair_dice fdice1;
 	string hero_name;
         do{
 		cout << "\nWelcome to The Old Ones\n";
@@ -24,7 +26,16 @@ int main()
 				cout << "\n place holder 2" << endl;
 				break;
 			case 3:
-				cout << "\n place holder 3" << endl;
+				cout << "\n Enter Number of Dice to be rolled: " << endl;
+				cin >> fdice1.number_of_die;
+				cout << "\n Enter Number of Sides on dice: " << endl;
+				cin >> fdice1.sided_dice;
+				cout << "\n Rolling Dice ..." << endl;
+				fdice1.roll_dice(fdice1.number_of_die,fdice1.sided_dice);
+				/* for(int i = 0; i < fdice1.number_of_die; ++i)
+				{
+					cout << "\n die #" << i << " :" << fdice1.die[i] << endl;
+				}*/
 				break;
 			case 4:
 				cout << "\n Exiting now" << endl;
