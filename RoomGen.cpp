b35a733cbd116b8ee484755f62df8eb5c0b51194
TyMKillerApps <hooch0022@gmail.com>
2022-10-03 18:47:56 -0500

    first iteration of RoomGen.cpp to be used in main.cpp room generations

---
+++ RoomGen.cpp
@@ -0,0 +1,13 @@
+#include "RoomGen.h"
+using namespace std;
+
+RoomGen::RoomGen()
+{
+    RoomGen::vectRooms = {{0,0}};
+    RoomGen::arrayXcords[0] = {0};
+    RoomGen::arrayYcords[0] = {0};
+    for(int i=0; i<40; i++)
+    {
+        
+    }
+}
